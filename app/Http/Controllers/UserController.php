35a986481dd941269a87e1cd96b4d3c9afe7eb38
gr8ref <refik@umisoft.ba>
2019-11-23 16:36:06 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,13 +9,17 @@ use Validator;
 class UserController extends Controller {
     //
     public function index()
-            {
+        {
+            $user = Auth::user();
+            if($user->is_admin) {
                 return response()->json(User::with(['orders'])->get());
             }
+        
+        }
     public function login(Request $request)
-             {
-                 $status = 401;
-                 $response = ['error' => 'Unauthorised'];
+        {
+            $status = 401;
+            $response = ['error' => 'Unauthorised'];
 
                  if (Auth::attempt($request->only(['email', 'password']))) {
                      $status = 200;
@@ -25,15 +29,14 @@ class UserController extends Controller {
                      ];
                  }
 
-                 return response()->json($response, $status);
-             }
+            return response()->json($response, $status);
+        }
 
-    public function show(User $user)
-            {
-                return response()->json($user);
-            }
+    public function show(User $user) {
+        return response()->json($user);
+        }
     public function showOrders(User $user)
-            {
-                return response()->json($user->orders()->with(['product'])->get());
-            }
+        {
+        return response()->json($user->orders()->with(['product'])->get());
+        }
 }
