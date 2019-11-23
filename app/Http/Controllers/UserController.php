c28650515d4d7a93f6d887740c6051a71e5a9f4a
gr8ref <refik@umisoft.ba>
2019-11-23 21:34:41 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,31 +9,45 @@ use Validator;
 class UserController extends Controller {
     //
     public function index()
-            {
-                return response()->json(User::with(['orders'])->get());
-            }
+    {
+        return response()->json(User::with(['orders'])->get());
+    }
     public function login(Request $request)
-             {
-                 $status = 401;
-                 $response = ['error' => 'Unauthorised'];
+    {
+        $status = 401;
+        $response = ['error' => 'Unauthorised'];
 
-                 if (Auth::attempt($request->only(['email', 'password']))) {
-                     $status = 200;
-                     $response = [
-                         'user' => Auth::user(),
-                         'token' => Auth::user()->createToken('MyApp')->accessToken,
-                     ];
-                 }
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            $status = 200;
+                $response = [
+                    'user' => Auth::user(),
+                    'token' => Auth::user()->createToken('MyApp')->accessToken,
+                ];
+        }
 
-                 return response()->json($response, $status);
-             }
+        return response()->json($response, $status);
+    }
+       
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();        
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
 
+   
+    public function is_admin(User $user)
+    {
+        return response()->json($user->is_admin);
+    }
+    
     public function show(User $user)
-            {
-                return response()->json($user);
-            }
+    {
+        return response()->json($user);
+    }
     public function showOrders(User $user)
-            {
-                return response()->json($user->orders()->with(['product'])->get());
-            }
+    {
+        return response()->json($user->orders()->with(['product'])->get());
+    }
 }
